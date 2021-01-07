749fc54f626372f14192f22c41c8c9a2bc9d409b
robbin.hsu <robbin.hsu@collaborate.com.tw>
2021-01-07 18:18:03 +0800

    產生SeedData

--- AttributeRouting/Migrations/20210107101216_seeddata.Designer.cs
+++ AttributeRouting/Migrations/20210107101216_seeddata.Designer.cs
@@ -4,14 +4,16 @@ using AttributeRouting.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AttributeRouting.Migrations
 {
     [DbContext(typeof(BookSampleContext))]
-    partial class BookSampleContextModelSnapshot : ModelSnapshot
+    [Migration("20210107101216_seeddata")]
+    partial class seeddata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- README.md
+++ README.md
@@ -15,8 +15,14 @@
         {
             Configuration = configuration;
         }
-        
 * 開啟 **Package Manager Console**，輸入以下指令：  
     ```csharp
     PM> Add-Migration initial
     ```
+* 在 DbContext 衍生類別覆寫 OnModelCreating()，寫入SeedData。  
+輸入以下指令，Migrations資料夾就會產生SeedData  
+    ```csharp 
+    PM> Add-Migration seeddata  
+    PM> Update-Database  
+    ```
+    
